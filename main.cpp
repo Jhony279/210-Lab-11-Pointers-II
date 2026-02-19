f5c3b95428d9514e1898c3c1c60af9426baaafa8
Jhony279 <lordjhony732@gmail.comm>
2026-02-18 17:19:44 -0800

    Finalized adding comments to code

--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 
 const int TEAM_AMOUNT = 2;
 
+/**
+ * @brief A struct that holds data on how the team performed in each 
+ *        match they played
+*/
 struct TeamPerformace {
     int pointsScored;   // Amount of Points scored by the team
     double cycleTime;   // Average amount of time in between each scored point
@@ -19,7 +23,7 @@ struct TeamPerformace {
 struct FRCTeam {
     int teamNumber;     // The team's #
     string teamName;    // Team's name
-    int matchesPlayed;  // # of matches played
+    int matchesPlayed;  // Number of matches played
     TeamPerformace *performace; // List of team's performace per match
     
     ~FRCTeam() {
@@ -33,23 +37,34 @@ void enterTeamData(FRCTeam*);
 void displayTeamInfo(FRCTeam*);
 
 /**
- * @brief - Description of main
+ * @brief Main function that creates a list of teams, then calls the functions to
+ *        enter the data for each team and display it
 */
 int main() {
+    // Create a dynamic list of teams
     FRCTeam *teamList = new FRCTeam[TEAM_AMOUNT];
 
+    // Enter the data for each team in the list
     for (int i = 0; i < TEAM_AMOUNT; i++){
         enterTeamData(&teamList[i]);
     }
 
+    // Display the data for each team in the list
     for (int i = 0; i < TEAM_AMOUNT; i++){
         displayTeamInfo(&teamList[i]);
     }
     return 0;
 }
 
+/**
+ * @brief Enters data in for a specific team
+ * @param tptr A pointer to the team struct that will have its data entered
+*/
 void enterTeamData(FRCTeam *tptr){
+    // Static variable to keep track of the team number being entered
     static int tmNbr = 1;
+
+    // Enter the data for the team
     cout << "Enter the data for team " << tmNbr << " on the list" << endl;
     cout << "Team Name: ";
     getline(cin, tptr->teamName);
@@ -57,6 +72,7 @@ void enterTeamData(FRCTeam *tptr){
     cout << "Matches Played: "; cin >> tptr->matchesPlayed;
     tptr -> performace = new TeamPerformace[tptr->matchesPlayed];
 
+    // Enter the data for each match the team played
     for (int i = 0; i < tptr->matchesPlayed; i++){
         tptr->performace[i].matchNumber = i;
         cout << "\nPoints scored for match " << i + 1<< ": ";
@@ -64,18 +80,23 @@ void enterTeamData(FRCTeam *tptr){
         cout << "Team's Cycle Time: ";
         cin >> tptr->performace[i].cycleTime;
     }
-    cin.ignore();
+    cin.ignore();   // Clear the input buffer for the next team
     cout << endl << endl;
-    tmNbr++;
+    tmNbr++;    // Increment the team number for the next team to be entered
 }
 
+/**
+ * @brief Displays the data for a specific team
+ * @param tptr A pointer to the team struct that will have its data displayed
+*/
 void displayTeamInfo(FRCTeam *tptr){
+    // Display the data for the team
     cout << "--- Team " << tptr->teamNumber << " summary ---";
     cout << "\nName: " << tptr->teamName;
     cout << "\nTeam# : " << tptr->teamNumber;
     cout << "\nMatches played: " << tptr->matchesPlayed << endl;
 
-    static int prtSpacing = 4;
+    // Display the data for each match the team played
     for (int i = 0; i < tptr->matchesPlayed; i++){
         cout << " -Performance for match " << i + 1 << ": " << endl;
         cout << "   Points scored: " 
